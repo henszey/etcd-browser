4294314a5c31f02858f70d46317545650bfb696d
Ryan Henszey <henszey@gmail.com>
2014-08-29 16:49:44 -0500

    Changed url structure and made raw stats button

--- etcdbrowser.js
+++ etcdbrowser.js
@@ -2,7 +2,10 @@
 var app = angular.module("app", ["xeditable"]);
 
 app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
-  $scope.urlPrefix = "http://localhost:4001/v2/keys";
+  var keyPrefix = '/v2/keys',
+      statsPrefix = '/v2/stats';
+  
+  $scope.urlPrefix = "http://localhost:4001";
 
   $scope.setActiveNode = function(node){
     $scope.activeNode = node;
@@ -14,12 +17,16 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
   }
 
   function errorHandler(data, status, headers, config){
-    $scope.error = "Request failed";
+    var message = data;
+    if(data.message) {
+      message = data.message;
+    }
+    $scope.error = "Request failed - " + message + " - " + config.url;
   }
 
   $scope.loadNode = function(node){
     delete $scope.error;
-    $http({method: 'GET', url: $scope.urlPrefix + node.key}).
+    $http({method: 'GET', url: $scope.urlPrefix + keyPrefix + node.key}).
       success(function(data) {
         prepNodes(data.node.nodes,node);
         node.nodes = data.node.nodes;
@@ -53,7 +60,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
     if(!name || name == "") return;
 
     $http({method: 'PUT', 
-    	   url: $scope.urlPrefix + node.key + (node.key != "/" ? "/" : "") + name, 
+    	   url: $scope.urlPrefix + keyPrefix + node.key + (node.key != "/" ? "/" : "") + name, 
     	   params: {"value": value}}).
     success(function(data) {
       $scope.loadNode(node);
@@ -63,7 +70,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
 
   $scope.updateNode = function(node,value){
     $http({method: 'PUT', 
-      url: $scope.urlPrefix + node.key, 
+      url: $scope.urlPrefix + keyPrefix + node.key, 
       params: {"value": value}}).
     success(function(data) {
       $scope.loadNode(node);
@@ -72,7 +79,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
   }
 
   $scope.deleteNode = function(node){
-    $http({method: 'DELETE', url: $scope.urlPrefix + node.key}).
+    $http({method: 'DELETE', url: $scope.urlPrefix + keyPrefix + node.key}).
     success(function(data) {
       $scope.loadNode(node.parent);
     }).
@@ -84,7 +91,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
     if(!dirName || dirName == "") return;
     dirName = $scope.formatDir(dirName);
     $http({method: 'PUT', 
-      url: $scope.urlPrefix + dirName + node.name, 
+      url: $scope.urlPrefix + keyPrefix + dirName + node.name, 
       params: {"value": node.value}}).
     error(errorHandler);
   }
@@ -93,7 +100,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
     var dirName = prompt("Enter Directory Name", "");
     if(!dirName || dirName == "") return;
     $http({method: 'PUT', 
-      url: $scope.urlPrefix + node.key + (node.key != "/" ? "/" : "") + dirName, 
+      url: $scope.urlPrefix + keyPrefix + node.key + (node.key != "/" ? "/" : "") + dirName, 
       params: {"dir": true}}).
     success(function(data) {
       $scope.loadNode(node);
@@ -107,7 +114,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
     dirName = $scope.formatDir(dirName);
     for(var key in node.nodes){
       $http({method: 'PUT', 
-        url: $scope.urlPrefix + dirName + node.nodes[key].name, 
+        url: $scope.urlPrefix + keyPrefix + dirName + node.nodes[key].name, 
         params: {"value": node.nodes[key].value}}).
       error(errorHandler);
     }
@@ -120,7 +127,7 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
     }
     if(!confirm("Are you sure you want to delete " + node.key)) return;
     $http({method: 'DELETE', 
-      url: $scope.urlPrefix + node.key + "?dir=true"}).
+      url: $scope.urlPrefix + keyPrefix + node.key + "?dir=true"}).
     success(function(data) {
       $scope.loadNode(node.parent);
     }).
@@ -143,6 +150,28 @@ app.controller('NodeCtrl', ['$scope','$http', function($scope,$http) {
       node.name = name;
       node.parent = parent;
     }
+  }
+  
+  $scope.loadStats = function(){
+    console.log("LOAD STATS");
+    $scope.stats = {};
+    $http({method: 'GET', url: $scope.urlPrefix + statsPrefix + "/store"}).
+    success(function(data) {
+      $scope.stats.store = JSON.stringify(data, null, " ");
+    }).
+    error(errorHandler);
+    delete $scope.storeStats;
+    $http({method: 'GET', url: $scope.urlPrefix + statsPrefix + "/leader"}).
+    success(function(data) {
+      $scope.stats.leader = JSON.stringify(data, null, " ");
+    }).
+    error(errorHandler);
+    delete $scope.storeStats;
+    $http({method: 'GET', url: $scope.urlPrefix + statsPrefix + "/self"}).
+    success(function(data) {
+      $scope.stats.self = JSON.stringify(data, null, " ");
+    }).
+    error(errorHandler);
   }
 
 }]);
