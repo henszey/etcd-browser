d3c82ed1930cca7d2c09edd773eeec7e351cb6af
Christoph Wiechert <wio@psitrax.de>
2015-01-14 13:28:18 +0100

    moved app to frontend folder and made default url = document.location.host

--- frontend/etcdbrowser.js
+++ frontend/etcdbrowser.js
@@ -8,7 +8,7 @@ app.controller('NodeCtrl', ['$scope','$http','$cookies', function($scope,$http,$
   if($cookies.urlPrefix){
     $scope.urlPrefix = $cookies.urlPrefix;
   } else {
-    $scope.urlPrefix = "http://localhost:4001";
+    $scope.urlPrefix = "http://" + document.location.host;
   }
 
   $scope.getPrefix = function() {
